a539b392f113f16340b388b6c43dbb41440552aa
Jonathan Mathew <jonathanmathew6548@gmail.com>
2023-01-19 20:42:43 -0500

    Had quotes page display quotes

--- src/components/pages/Quotes.js
+++ src/components/pages/Quotes.js
@@ -1,3 +1,28 @@
+import React,{useState,useEffect} from "react"
 export default function Quotes(){
-    return <h1>Quotes</h1>
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("https://quotesondesign.com/wp-json/wp/v2/posts/")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        });
+    }, []);
+
+
+    return( 
+    
+        <div>
+        <h1>Quotes</h1>
+        {data.map((item) => (
+          <div key={item.id}>
+            {item.content.rendered}
+            {item.title.rendered}
+          </div>
+        ))}
+      </div>
+    )
 } 
+
