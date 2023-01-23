45ed9e01e89fc51bf05b5db4c83ffc779a46868b
Jonathan Mathew <jonathanmathew6548@gmail.com>
2023-01-23 13:53:49 -0500

    here is the message

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ export default function About(){
     return(
     <div>
     <h1>About</h1>
-    <body>hello my name is johnny and i am the front end developer</body>
+    <body>Hello my name is Johnny and i am the front end developer</body>
     </div>
     )
 } 
